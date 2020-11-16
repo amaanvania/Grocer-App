75f3d5fd98306d5205a675ef6475156b01453c58
Amaan Vania <amaanv@hotmail.com>
2020-11-15 21:37:24 -0500

    refactoring

--- GrocerApp/src/main/java/grocer/GrocerApplication.java
+++ GrocerApp/src/main/java/grocer/GrocerApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GrocerApplication {
     public static void main(String... args) {
-        SpringApplication.run(GrocerApplication.class, args);
+        SpringApplication.run(GrocerApplication.class,args);
     }
 }
